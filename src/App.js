72493059d1e7f7aff150b535ff1e1402b8b126ad
kunalAkhade <bialk@LAPTOP-C3TGDGH0>
2023-01-08 20:41:38 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Add Inventory Items</h1>
+      <input className="brand-name" type="text" placeholder="Enter Brand Name"/>
+       <input className="mobile-name" type="text" placeholder="Enter Mobile Name"/>
+       <label className="label" for="Ram">Select Ram Size:</label>
+
+            <select name="Ram" id="Ram">
+                <option value="4GB"> 4GB</option>
+                <option value="6GB"> 6GB</option>
+                <option value="8GB"> 8GB</option>
+                <option value="12GB">12GB</option>
+            </select>
+            <div></div>
+            <label className="Storage" for="Storage">Select Storage Size:</label>
+
+               <select name="Storage" id="Storage" placeholder="Enter">
+                <option value="64GB"> 64GB</option>
+                <option value="128GB">128GB</option>
+                <option value="256GB">256GB</option>
+                <option value="512GB">512GB</option> 
+              </select>    
+      <input className="Button" type="submit" value="Add"/>      
     </div>
   );
 }
